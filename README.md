7b5a790d6b8c5425b8e56e02f9bd794c8eb0af96
MLOPEZMALET <melanie.lopezmalet@gmail.com>
2020-01-10 16:13:46 +0100

    new xml

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ Le projet est constitué des éléments suivants:
 
 	- dans data: 
 		-> les données originales en csv (kaggle_steam-store_games) 
-		-> le csv ne contenant que les données choisies (data.csv)
-		-> le fichier XML résultant du formatage du csv filtré (data.xml)
-		-> un fichier txt décrivant nos choix de modélisation (rapport.txt)
+		-> le csv ne contenant que certaines données choisies après un premier filtrage (data.csv)
+		-> le fichier XML final résultant du formatage du csv data.csv (data.xml)
+		-> un fichier txt décrivant nos choix de formalisation (rapport.txt)
 
 	- dans web: 
 		-> les pages html (explication.html et resultat.html)
@@ -25,7 +25,7 @@ Le projet est constitué des éléments suivants:
 
 	- dans scripts:
 		-> le script pour fusionner tous les csv du jeu de données (merge_all_csv.py)
-		-> le script pour transformer le csv en xml (csv_to_xml.py)
+		-> le script pour transformer data.csv en xml (csv_to_xml.py)
 		-> le notebook qui a servi a élaborer les graphes avec Plotly
 
 	- dans transformation:
--- scripts/csv_to_xml.py
+++ scripts/csv_to_xml.py
@@ -13,21 +13,20 @@ from lxml import etree as ET
 import re
 import unidecode
 
-def csv_to_xml(input_path=f"..{os.sep}data{os.sep}data.csv", 
-         output_path=f"..{os.sep}data{os.sep}data.xml"):
+def csv_to_xml(input_path=f"..{os.sep}data{os.sep}data.csv",
+         output_path=f"..{os.sep}data{os.sep}data_2.xml"):
 
     # load drataset
     df = pd.read_csv(input_path, index_col="Index", encoding="utf8")
 
     # build tree ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     root = ET.Element("games")
-    
+
     for i in range(len(df)):
         row = df.iloc[i]
         game = ET.SubElement(root, "game", id=str(row.id))
         ET.SubElement(game, "name").text = str(row[1])
         ET.SubElement(game, "release_date").text = str(row.release_date)
-        ET.SubElement(game, "english").text = str(row.english)
         ET.SubElement(game, "developer").text = str(row.developer)
         ET.SubElement(game, "publisher").text = str(row.publisher)
         ET.SubElement(game, "platforms").text = str(row.platforms)
@@ -36,35 +35,13 @@ def csv_to_xml(input_path=f"..{os.sep}data{os.sep}data.csv",
         ratings = ET.SubElement(game, "ratings")
         ET.SubElement(ratings, "positive").text = str(row.positive_ratings)
         ET.SubElement(ratings, "negative").text = str(row.negative_ratings)
-        playtime = ET.SubElement(game, "playtime")
-        ET.SubElement(playtime, "average_playtime").text = str(row.average_playtime)
-        ET.SubElement(playtime, "median_playtime").text = str(row.median_playtime)
         ET.SubElement(game, "price").text = str(row.price)
-        #ET.SubElement(game, "description").text = str(row.short_description)
-        
-        # cleaning about_the_game #############################################
-        about_the_game = re.sub(r"<.+?>", " ", row.about_the_game)
-        about_the_game = re.sub("[ \t\n\r\f\v]+", " ", about_the_game)
-        about_the_game = re.sub("&quot;", "\"", about_the_game)
-        about_the_game = re.sub("’", "'", about_the_game)
-        about_the_game = re.sub("[]", "", about_the_game)
-        about_the_game = re.sub("\-{2,}", "", about_the_game)
-        about_the_game = re.sub(". \x08\x08", ". ", about_the_game)
-        try:
-            ET.SubElement(game, "about_the_game").text = about_the_game
-        except:
-            print(about_the_game.encode("utf8"))
-            #print(unidecode.unidecode(about_the_game))
-            print("+++++++++++++++++++++")
-            ET.SubElement(game, "about_the_game").text = about_the_game.encode("utf8")
-        #######################################################################   
-            
+
     tree = ET.ElementTree(root)
     tree.write(output_path, pretty_print=True)
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    
+
     print("ok")
 
-        
-csv_to_xml()
 
+csv_to_xml()
